8f3dbd9acfbc7ca1f08f62ae54b56685e14d11df
Adrian Rak <adrrak@tlen.pl>
2017-10-30 21:35:11 +0100

    0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 3;
-var b = 1;
+var a = 0;
+var b = 0;
 var value = (a * a) - (2 * a * b) + (b * b);
 
 console.log(value);
@@ -9,7 +9,7 @@ if (value > 0) {
 } else if (value < 0) {
     console.log('wynik ujemny');
 } else {
-    console.log('0')
+    console.log('Wynik = 0')
 }
 
 
