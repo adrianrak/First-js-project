4a4464ae2d38c2761aa7c3b9d17142f3380cb43b
Adrian Rak <adrrak@tlen.pl>
2017-11-03 22:53:32 +0100

    kodilla-9-4

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var drawTree = 7; 
+for (var i = 0 ; i <= drawTree ; i++) {
+    var star = ''; 
+
+    for (var j = 0 ; j <= i ; j++) {
+        var star = star += '*';
+    }
+console.log(star);
+}
+    
