76301d7188b249eb7f5e7eafd0a6e858ba039c1d
Adrian Rak <adrrak@tlen.pl>
2017-11-04 12:53:05 +0100

    add split

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'triceratops';
 var dinosaurUpperCased = dinosaur.toUpperCase();
-var textChange = text.replace('Velociraptor', [dinosaurUpperCased]);
+var splitText = text.split(' ', 1);
+var textChange = text.replace([splitText], [dinosaurUpperCased]);
 
 console.log(textChange.substr(0, textChange.length / 2));
