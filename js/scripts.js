3d27b7cf8a4e5cee97d6db597fd7ef3235603700
Adrian Rak <adrrak@tlen.pl>
2017-11-05 22:59:05 +0100

    add space

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 var drawTree = 7; 
-for (var i = 0 ; i <= drawTree ; i++) {
+for (var i = 1 ; i <= drawTree ; i++) {
     var star = ''; 
-
-    for (var j = 0 ; j <= i ; j++) {
-        var star = star += '*';
+    for (var j = drawTree - 1 ; j >= i ; --j) {
+        var star = star += ' ';
     }
+    for (var j = 1 ; j <= (2 * i - 1) ; j++) {
+        var star = star += '*';     
+    } 
 console.log(star);
 }
     
