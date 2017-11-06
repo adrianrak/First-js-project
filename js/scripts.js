f7fdba5d17467a0c1892273309409b4664dd1d42
Adrian Rak <adrrak@tlen.pl>
2017-11-06 11:35:16 +0100

    add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-var drawTree = 7; 
-for (var i = 1 ; i <= drawTree ; i++) {
-    var star = ''; 
-    for (var j = drawTree - 1 ; j >= i ; --j) {
-        var star = star += ' ';
+function drawTree(line) { 
+    for (var i = 1 ; i <= line ; i++) {
+        var star = ''; 
+        for (var j = line - 1 ; j >= i ; --j) {
+            var star = star += ' ';
+        }
+        for (var j = 1 ; j <= (2 * i - 1) ; j++) {
+            var star = star += '*';     
+        } 
+    console.log(star);
     }
-    for (var j = 1 ; j <= (2 * i - 1) ; j++) {
-        var star = star += '*';     
-    } 
-console.log(star);
 }
-    
+drawTree(30);
