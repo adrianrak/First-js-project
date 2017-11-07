e9b9c87d8ee7c19c47bd04790a05e6eefd37b75f
Adrian Rak <adrrak@tlen.pl>
2017-11-07 20:59:42 +0100

    innerElement

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
     var element = document.createElement('li');
     var addList = document.getElementsByTagName('li');
-    
+    var innerElement = 'item ' +  addList.length;
     if (addList.length >= 0 && addList.length%2 == 0) {
-        element.innerHTML = ('item ' +  addList.length).fontcolor("red");
+        element.innerHTML = innerElement.fontcolor("red");
     } else {
-        element.innerHTML = 'item ' +  addList.length;
+        element.innerHTML = innerElement;
     }
 
     list.appendChild(element);
