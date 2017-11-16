947bb7d9a076894bb4b448e32ef55f13574d3934
Adrian Rak <adrrak@tlen.pl>
2017-11-16 09:02:42 +0100

    kodilla-10-2

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
 
 var ignored = ['#js-span1', '#js-span5'];
 
-    var span = $('span');
-// for (var span = 0 ; span < 3 ; span++) {
-//     $(span).css('color', 'red');
-// }   
-
-// console.log(ignored);
-// function red() {
-//     $(span).css('color', 'red');
-// }
-// red();
+var span = $('span');
 span.each(function(index, element) {
-    if (index == ['#js-span1']) {
+    if (element !== ignored) {
         $(element).css('color', 'red');
-    }
+    };
 });
 
 
