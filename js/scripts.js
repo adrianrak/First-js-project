57fd8c8adac426da6736c7c7f629d1f8dc2de0f4
Adrian Rak <adrrak@tlen.pl>
2017-11-20 21:45:39 +0100

    add this.getWarrantyCost()

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
-function Phone(brand, price, color, warranty) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-	this.warranty = price / 10;
+class Phone {
+	constructor(brand, price, color) {
+		this.brand = brand;
+		this.price = price;
+		this.color = color;
+		
+	}
+	getWarrantyCost() {
+		return this.price / 10;
+	} 	
+	
+	printInfo() {
+		console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.' + ' Cost of extended warranty: ' + this.getWarrantyCost());
+	}
 }
-Phone.prototype.printInfo = function() {
-	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.' + ' Cost of extended warranty: ' + this.warranty);
-}
+
 var samsungGalaxyS6 = new Phone('Samsung', 1200, 'black');
 var iPhone6S = new Phone('Apple', 2250, 'silver');
 var onePlusOne = new Phone('OnePlus', 1800, 'white');
@@ -14,6 +21,7 @@ var onePlusOne = new Phone('OnePlus', 1800, 'white');
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 onePlusOne.printInfo();
+
 
 //Proszę dorób jeszcze wyświetlanie ceny przedłużonej gwarancji, która równa jest 10% ceny telefonu. 
 //Stwórz do tego metodę getWarrantyCost(), którą użyjesz potem w printInfo()
