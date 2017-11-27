d55f01f41a6292bfe232244b4acbeccce37e471c
Adrian Rak <adrrak@tlen.pl>
2017-11-27 10:33:17 +0100

    Search Countries

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,25 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+	var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
+}
+function showCountriesList(resp) {
+	countriesList.empty();
+  }
+  function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item) {
+		$('<li>').text('country name: ' + item.name).appendTo(countriesList);
+		$('<li>').text('capital: ' + item.capital).appendTo(countriesList);
+		$('<li>').text('language: ' + item.languages).appendTo(countriesList);
+    });
+}
